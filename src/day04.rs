69500c93b98ab981c6f58ca9e715def32ef50ca0
Matthew Lynch <mslynch95@gmail.com>
2018-09-16 15:48:20 -0500

    day 4 solution

--- src/day04.rs
+++ src/day04.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+
 use std::cmp::Ordering;
 use std::collections::BTreeMap;
 use std::fs::File;
@@ -14,7 +16,8 @@ pub fn run(filename: Option<&str>) {
     let input = reader.lines().map(Result::unwrap).collect::<Vec<String>>();
 
     println!("part 1: {}", real_room_id_sum(&input));
-    // println!("part 2: {}", actual_vertical_triangle_count(&input));
+    // find_north_pole_sector(&input);
+    println!("part 2: {}", find_north_pole_sector(&input));
 }
 
 #[derive(PartialEq, Debug)]
@@ -44,9 +47,51 @@ pub fn real_room_id_sum(input: &[String]) -> usize {
     input
         .iter()
         .map(|input| parse_room(input))
-        .filter(is_real)
+        .filter(Room::is_real)
         .map(|room| room.id)
         .sum()
+}
+
+pub fn find_north_pole_sector(input: &[String]) -> usize {
+    let (id, _decrypted) = input
+        .iter()
+        .map(|input| parse_room(input))
+        .filter(Room::is_real)
+        .map(|room| (room.id, room.decrypt()))
+        .find(|(_id, decrypted)| decrypted.contains("northpole"))
+        .unwrap();
+    id
+}
+
+impl<'a> Room<'a> {
+    fn is_real(&self) -> bool {
+        let mut chars_by_count = BTreeMap::new();
+        for c in self.words.iter().flat_map(|s| s.chars()) {
+            let count = chars_by_count.entry(c).or_insert(0);
+            *count += 1;
+        }
+        let sorted = sorted(chars_by_count.iter().map(|(c, count)| CharCount {
+            character: *c,
+            count: *count,
+        }));
+        sorted
+            .iter()
+            .map(|char_count| char_count.character)
+            .take(5)
+            .collect::<String>()
+            == self.checksum
+    }
+
+    fn decrypt(&self) -> String {
+        let shift = (self.id % 26) as u8;
+        self.words
+            .iter()
+            .map(|word| {
+                word.chars()
+                    .map(|c| shift_char(c, shift))
+                    .collect::<String>()
+            }).join(" ")
+    }
 }
 
 fn parse_room(room: &str) -> Room {
@@ -62,22 +107,13 @@ fn parse_room(room: &str) -> Room {
     }
 }
 
-fn is_real(room: &Room) -> bool {
-    let mut chars_by_count = BTreeMap::new();
-    for c in room.words.iter().flat_map(|s| s.chars()) {
-        let count = chars_by_count.entry(c).or_insert(0);
-        *count += 1;
+fn shift_char(c: char, shift: u8) -> char {
+    let shifted = c as u8 + shift;
+    if shifted <= 122 {
+        shifted as char
+    } else {
+        (shifted - 26) as char
     }
-    let sorted = sorted(chars_by_count.iter().map(|(c, count)| CharCount {
-        character: *c,
-        count: *count,
-    }));
-    sorted
-        .iter()
-        .map(|char_count| char_count.character)
-        .take(5)
-        .collect::<String>()
-        == room.checksum
 }
 
 #[cfg(test)]
@@ -93,6 +129,36 @@ mod tests {
             checksum: "abxyz".to_string(),
         };
         assert_eq!(expected_room, parse_room(input));
+    }
+
+    #[test]
+    fn test_shift_char_1() {
+        assert_eq!('a', shift_char('a', 0));
+    }
+
+    #[test]
+    fn test_shift_char_2() {
+        assert_eq!('e', shift_char('a', 4));
+    }
+
+    #[test]
+    fn test_shift_char_3() {
+        assert_eq!('a', shift_char('z', 1));
+    }
+
+    #[test]
+    fn test_shift_char_4() {
+        assert_eq!('z', shift_char('y', 1));
+    }
+
+    #[test]
+    fn decryption_test() {
+        let room = Room {
+            words: vec!["qzmt", "zixmtkozy", "ivhz"],
+            id: 343,
+            checksum: "".to_string(),
+        };
+        assert_eq!("very encrypted name", room.decrypt());
     }
 
 }
