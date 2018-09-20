9f94f96b01d55b4e94d64e89118190bfbfdaa93c
Matthew Lynch <matthew.lynch@deluxe.com>
2018-09-19 19:19:32 -0500

    day 5 solution

--- src/day05.rs
+++ src/day05.rs
@@ -4,18 +4,20 @@ use std::io::prelude::*;
 use crypto::digest::Digest;
 use crypto::md5::Md5;
 
-/// Runs the problems for day 1.
+/// Runs the problems for day 5.
 pub fn run(filename: Option<&str>) {
-    println!("Day 1: No Time for a Taxicab");
+    println!("Day 5: How About a Nice Game of Chess?");
     let mut file = File::open(filename.unwrap_or("data/day05.txt")).expect("file not found");
     let mut contents = String::new();
     file.read_to_string(&mut contents)
         .expect("something went wrong reading the file");
 
     println!("part 1: {}", generate_password(&contents));
+    println!("part 2: {}", generate_password_2(&contents));
 }
 
-fn interesting_hash(index: usize, input: &str) -> Option<char> {
+/// If the index and input make an interesting hash, return the character and position.
+fn interesting_hash_digit(index: usize, input: &str) -> Option<(char, char)> {
     let mut md5 = Md5::new();
     let prehash = &format!("{}{}", input, index);
     md5.input_str(prehash);
@@ -26,14 +28,40 @@ fn interesting_hash(index: usize, input: &str) -> Option<char> {
             return None;
         }
     }
-    hash_chars.next()
+    Some((hash_chars.next().unwrap(), hash_chars.next().unwrap()))
 }
 
+/// Generates passwords, with the 5th digit being the next character.
 pub fn generate_password(input: &str) -> String {
     (0..)
-        .filter_map(|i| interesting_hash(i, input))
+        .filter_map(|i| interesting_hash_digit(i, input))
+        .map(|(password_char, _)| password_char)
         .take(8)
         .collect()
+}
+
+/// Converts an array password to a String.
+fn password_to_string(password: [Option<char>; 8]) -> String {
+    password.iter().map(|c| c.unwrap_or('_')).collect()
+}
+
+/// Generates passwords, with the 5th digit being the position and the 6th being the character.
+pub fn generate_password_2(input: &str) -> String {
+    let mut password: [Option<char>; 8] = [None; 8];
+    let mut digits_calculated = 0;
+    'outer: for i in 0.. {
+        if let Some((position_char, character)) = interesting_hash_digit(i, input) {
+            let position = position_char.to_digit(16).unwrap() as usize;
+                if position < 8 && password[position] == None {
+                    password[position] = Some(character);
+                    digits_calculated += 1;
+                }
+                if digits_calculated == 8 {
+                    break 'outer;
+                }
+        }
+    }
+    password_to_string(password)
 }
 
 #[cfg(test)]
@@ -41,8 +69,10 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_interesting_hash() {
-        assert_eq!('1', interesting_hash(3231929, "abc").unwrap());
+    fn test_interesting_hash_digit() {
+        let (first, second) = interesting_hash_digit(3231929, "abc").unwrap();
+        assert_eq!('1', first);
+        assert_eq!('5', second);
     }
 
 }
--- tests/day05.rs
+++ tests/day05.rs
@@ -1,9 +1,13 @@
 extern crate adventofcode2016;
 
-use adventofcode2016::day05::generate_password;
+use adventofcode2016::day05::{generate_password, generate_password_2};
 
 #[test]
 fn password_generation_test() {
     assert_eq!("18f47a30", generate_password("abc"));
 }
 
+#[test]
+fn password_generation_2_test() {
+    assert_eq!("05ace8e3", generate_password_2("abc"));
+}
