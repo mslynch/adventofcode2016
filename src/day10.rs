66fb93d1a2fe00181db78fc3cea234926f2a4b52
Matthew Lynch <mslynch95@gmail.com>
2018-12-22 23:57:24 -0600

    day 10 solution

--- src/day10.rs
+++ src/day10.rs
@@ -1,10 +1,8 @@
-// use std::collections::HashMap;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::str;
 use im::hashmap::HashMap;
-// use itertools::Itertools;
 
 /// Runs the solutions for day 10.
 pub fn run(filename: Option<&str>) {
@@ -13,28 +11,38 @@ pub fn run(filename: Option<&str>) {
     let reader = BufReader::new(file);
 
     let input: Vec<String> = reader.lines().map(Result::unwrap).collect();
-    let (instructions, mut initial_state) = parse_input(&input);
-    // let (lit, screen) = run_instructions(&input);
-    println!("part 1: {}", run_bots(&instructions, &mut initial_state, None));
-    // println!("part 2: ");
-    // display(&screen);
+    let (instructions, initial_state) = parse_input(&input);
+    let (state, comparebot) = run_bots(instructions, initial_state, &[17, 61], None);
+    println!("part 1: {}", comparebot.unwrap());
+    
+    let bins = state.bins;
+    let product = (0..3).fold(1, |product, bin_id| product * bins.get(&bin_id).unwrap());
+    println!("part 2: {}", product);
 }
 
 #[derive(Clone)]
-enum OutputType {
+pub enum OutputType {
     Bot,
     Bin,
 }
 
 #[derive(Clone)]
-struct Bot {
-    id: usize,
-    output_type: OutputType,
+pub struct Bot {
+    pub id: usize,
+    pub output_type: OutputType,
 }
 
+#[derive(Debug, PartialEq)]
+pub struct State {
+    pub bots: HashMap<usize, Vec<usize>>,
+    pub bins: HashMap<usize, usize>,
+}
+
+// type Instructions = HashMap<usize, Vec<Bot>>;
+
 /// Returns a tuple of the bot instructions and the initial state.
-fn parse_input(input: &[String]) -> (HashMap<usize, Vec<Bot>>, HashMap<usize, Vec<usize>>) {
-    input.iter().fold((HashMap::new(), HashMap::new()), |(bots, values), line| {
+fn parse_input(input: &[String]) -> (HashMap<usize, Vec<Bot>>, State) {
+    let (instructions, state_bots) = input.iter().fold((HashMap::new(), HashMap::new()), |(bots, values), line| {
         let mut split = line.split(' ');
         match split.next().unwrap() {
             "bot" => {
@@ -69,7 +77,8 @@ fn parse_input(input: &[String]) -> (HashMap<usize, Vec<Bot>>, HashMap<usize, Ve
                                 output_type: high_type,
                             },
                         ]), 
-                        values)
+                    values
+                )
 
             }
             _value => {
@@ -82,7 +91,8 @@ fn parse_input(input: &[String]) -> (HashMap<usize, Vec<Bot>>, HashMap<usize, Ve
                 (bots, values.update_with(bot, vec![value], |existing, _new| ordered_vec(value, existing[0])))
             }
         }
-    })
+    });
+    (instructions, State { bots: state_bots, bins: HashMap::new() })
 }
 
 
@@ -94,61 +104,70 @@ fn ordered_vec(a: usize, b: usize) -> Vec<usize> {
     }
 }
 
-fn run_bots(
-    bot_instructions: &HashMap<usize, Vec<Bot>>,
-    bot_state: &HashMap<usize, Vec<usize>>,
+/// Determines the end state and the bot responsible for comparing two different values
+pub fn run_bots<S: ::std::hash::BuildHasher>(
+    bot_instructions: HashMap<usize, Vec<Bot>, S>,
+    state: State,
+    comparebot_values: &[usize],
     bot_comparing_61_17: Option<usize>
-) -> usize {
+) -> (State, Option<usize>) {
+
+    let bots = state.bots.clone();
+    let bins = state.bins.clone();
 
     // find bots that are full and not full
-    let (not_full, full) = bot_state
-        .iter()
+    let (not_full, full): (HashMap<usize, Vec<usize>>, HashMap<usize, Vec<usize>>) = bots
+        .into_iter()
         .fold((HashMap::new(), HashMap::new()), |(not_full, full), (bot, values)| {
             match values.len() {
                 2 => (not_full, full.update(bot, values)),
                 _ => (not_full.update(bot, values), full),
             }
         });
+    
+    if full.is_empty() {
+        (state, bot_comparing_61_17)
+    } else {
+        // distribute the full bots' values and find the comparebot if it exists
+        let (non_empty, new_bins, new_comparebot) = full
+            .iter()
+            .fold((not_full, bins, bot_comparing_61_17), |(bot_merges, bin_merges, comparebot), (bot, values)| {
+            // merge a bot's low and high into other bots
+            let (new_bot_merges, new_bin_merges) = values
+                .iter()
+                .zip(bot_instructions.get(bot).unwrap().iter()).fold((bot_merges, bin_merges), |(bot_merge_acc, bin_merge_acc), (value, goes_to)| {
+                match goes_to.output_type {
+                    OutputType::Bot => (
+                        bot_merge_acc.update_with(goes_to.id, vec![*value], |existing, _new| ordered_vec(*value, existing[0])),
+                        bin_merge_acc
+                    ),
+                    OutputType::Bin => (
+                        bot_merge_acc,
+                        bin_merge_acc.update(goes_to.id, *value)
+                    )
+                }
+            });
 
-    // compute bots that have values after this iteration
-    let (non_empty, new_compare_bot) = full.iter().fold((not_full, bot_comparing_61_17), |(merges, compare_bot), (bot, values)| {
-        // merge the low and high values into the single-value bots
-        // let x: usize = bot_instructions.get(bot).unwrap();
-        let new_merges = values.iter().zip(bot_instructions.get(bot).unwrap().iter()).fold(merges, |merge_acc, (value, goes_to)| {
-            merge_acc.update_with(&goes_to.id, &vec![*value], |existing, _new| &ordered_vec(*value, existing[0]))
+            let maybe_new_comparebot = comparebot.or_else(|| {
+                if values.as_slice() == comparebot_values {
+                    Some(*bot)
+                } else {
+                    None
+                }
+            });
+
+            (new_bot_merges, new_bin_merges, maybe_new_comparebot)
         });
 
-        let maybe_new_compare_bot = compare_bot.or_else(|| {
-            if values[0] == 17 && values[1] == 61 {
-                Some(**bot)
-            } else {
-                None
-            }
-        });//});
+        // the previously full bots will have zero for the next iteration
+        let empty = full.iter().map(|(bot, _value)| (*bot, vec![])).collect();
 
-        (new_merges, maybe_new_compare_bot)
-    });
+        let new_bots = non_empty.union(empty);
+        let new_state = State {
+            bots: new_bots,
+            bins: new_bins,
+        };
 
-    // the previously full bots will have zero for the next iteration
-    let empty = full.iter().map(|(bot, _value)| (*bot, &vec![])).collect();
-
-    let new_bot_state = non_empty.union(empty);
-
-    // let states_by_length = bot_state.iter().group_by(|(_id, low_and_high)| low_and_high.len());
-
-    // bot_comparing_61_17.unwrap();
-    8
-}
-
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn rotate_row_test_2() {
-
-        // assert_eq!(expected, screen);
+        run_bots(bot_instructions, new_state, comparebot_values, new_comparebot)
     }
-
 }
