443948de61668df0fcca1f72f62a5ded26c71c55
Matthew Lynch <mslynch95@gmail.com>
2019-11-24 09:16:01 -0600

    remove comment

--- tests/day16.rs
+++ tests/day16.rs
@@ -8,5 +8,9 @@ use adventofcode2016::day15::run;
 fn pad_test() {
     let mut file = File::open("data/day15/test.dat").expect("File not found!");
     let result = run(&mut file);
-    assert_eq!("5", result.part1, "can retrieve capsule at time=5");
+    assert_eq!("01100", result.part1, "the correct checksum is calculated");
+    // assert_eq!(
+    //     "22551", result.part2,
+    //     "64th one-time pad is calculated hashing 2017 times"
+    // );
 }
