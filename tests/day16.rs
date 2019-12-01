19b216418c5b4b9aacdad11d3d4fe0c096e55022
Matthew Lynch <mslynch95@gmail.com>
2019-12-01 16:17:28 -0600

    fix day 16 test

--- tests/day16.rs
+++ tests/day16.rs
@@ -2,11 +2,11 @@ extern crate adventofcode2016;
 
 use std::fs::File;
 
-use adventofcode2016::day16::run_with_disk_space;
+use adventofcode2016::day16::run_test;
 
 #[test]
 fn checksum_test() {
     let mut file = File::open("data/day16/test.dat").expect("File not found!");
-    let result = run_with_disk_space(&mut file);
+    let result = run_test(&mut file);
     assert_eq!("01100", result.part1, "the correct checksum is calculated");
 }
